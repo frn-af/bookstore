8e71de84a208f520a55f62a22d3e2c1aad09ca46
frn-af <91farhanalfathra@gmail.com>
2024-03-28 00:53:26 +0700

    cliet: update login and register ui

--- apps/client/src/page/Login.tsx
+++ apps/client/src/page/Login.tsx
@@ -1,5 +1,28 @@
+import { Button } from "@repo/ui/components/ui/button";
+import { Input } from "@repo/ui/components/ui/input";
+
 const Login = () => {
-  return <div>Login</div>;
+  const loginUser = () => {
+    return (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log("Logging in user...");
+    };
+  };
+  return (
+    <div className="container flex flex-col items-center">
+      <h1 className="w-1/4 text-5xl font-black">Login to your account</h1>
+      <form
+        className="w-1/4 font-bold flex flex-col mt-10"
+        onSubmit={loginUser()}
+      >
+        <label>Email</label>
+        <Input type="email" placeholder="Enter your email..." />
+        <label>Password</label>
+        <Input type="password" placeholder="Enter your password..." />
+        <Button className="font-bold mt-4">Login</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- apps/client/src/page/Register.tsx
+++ apps/client/src/page/Register.tsx
@@ -1,5 +1,32 @@
+import { Button } from "@repo/ui/components/ui/button";
+import { Input } from "@repo/ui/components/ui/input";
+
 const Register = () => {
-  return <div>Register</div>;
+  const RegisterUser = () => {
+    return (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log("Registering user...");
+    };
+  };
+  return (
+    <div className="container flex flex-col items-center">
+      <h1 className="w-full md:w-1/4 text-5xl font-black">
+        Register your account
+      </h1>
+      <form
+        className="w-full md:w-1/4 font-bold flex flex-col mt-10"
+        onSubmit={RegisterUser()}
+      >
+        <label>Username</label>
+        <Input type="text" placeholder="Enter your username..." />
+        <label>Email</label>
+        <Input type="email" placeholder="Enter your email..." />
+        <label>Password</label>
+        <Input type="password" placeholder="Enter your password..." />
+        <Button className="font-bold mt-4">Login</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
